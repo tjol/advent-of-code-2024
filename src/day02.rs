0b044091131cf0cbc666490a9ce24cf6d1cc694a
Thomas Jollans <thomas@soundcell.nl>
2024-12-03 09:20:42 +0100

    finally fixed day2

--- src/day02.rs
+++ src/day02.rs
@@ -4,18 +4,12 @@ pub fn day02part1(input: &str) -> i32 {
     input
         .split('\n')
         .filter(|line| {
-            if let itertools::MinMaxResult::MinMax(mindiff, maxdiff) = line
+            let levels = line
                 .trim()
                 .split_ascii_whitespace()
                 .filter_map(|s| s.parse().ok())
-                .tuple_windows()
-                .map(|(a, b): (i32, i32)| a - b)
-                .minmax()
-            {
-                (mindiff >= 1 && maxdiff <= 3) || (mindiff >= -3 && maxdiff <= -1)
-            } else {
-                false
-            }
+                .collect::<Vec<i32>>();
+            valid1(&levels, 1) || valid1(&levels, -1)
         })
         .count() as i32
 }
@@ -32,37 +26,51 @@ pub fn day02part2(input: &str) -> i32 {
         .filter(|v| !v.is_empty());
 
     level_lines
-        .filter(|levels| min_invalid(levels) <= 1)
+        .filter(|levels| valid2(&levels, 1) || valid2(&levels, -1))
         .count() as i32
 }
 
-fn count_invalid(levels: &[i32], sign: i32) -> i32 {
-    if levels.is_empty() {
-        return 0;
+fn valid1(levels: &[i32], sign: i32) -> bool {
+    match levels
+        .into_iter()
+        .tuple_windows()
+        .map(|(a, b)| (b - a) * sign)
+        .minmax()
+    {
+        itertools::MinMaxResult::MinMax(mindiff, maxdiff) => mindiff >= 1 && maxdiff <= 3,
+        itertools::MinMaxResult::OneElement(diff) => diff >= 1 && diff <= 3,
+        itertools::MinMaxResult::NoElements => false,
     }
-    let mut invalid_count = 0;
-    let mut prev = levels[0];
-    for level in &levels[1..] {
-        let diff = (*level - prev) * sign;
-        if diff >= 1 && diff <= 3 {
-            prev = *level;
-        } else {
-            invalid_count += 1;
-        }
-    }
-    invalid_count
 }
 
-fn min_invalid(levels: &[i32]) -> i32 {
-    [
-        count_invalid(levels, 1),
-        count_invalid(levels, -1),
-        count_invalid(&levels[1..], 1) + 1,
-        count_invalid(&levels[1..], -1) + 1,
-    ]
-    .into_iter()
-    .min()
-    .unwrap()
+fn valid2(levels: &[i32], sign: i32) -> bool {
+    if levels.is_empty() {
+        return false;
+    }
+    let mut had_invalid = false;
+    let mut prev = levels[0];
+
+    for i in 1..levels.len() {
+        let diff = (levels[i] - prev) * sign;
+        if diff < 1 || diff > 3 {
+            if had_invalid {
+                return false;
+            } else if i >= 2 {
+                // check if removing the previous level might help
+                let diff2 = (levels[i] - levels[i - 2]) * sign;
+                if diff2 >= 1 && diff2 <= 3 && valid1(&levels[i..], sign) {
+                    return true;
+                }
+            } else if i == 1 && valid1(&levels[i..], sign) {
+                return true;
+            }
+            had_invalid = true;
+        } else {
+            // valid - accept
+            prev = levels[i];
+        }
+    }
+    return true;
 }
 
 #[cfg(test)]
@@ -97,12 +105,13 @@ mod test {
 
     #[test]
     fn part2_edge_cases() {
-        assert_eq!(min_invalid(&[1, 2, 3, 4, 5]), 0);
-        assert_eq!(min_invalid(&[1, 2, 3, 100, 4, 5]), 1);
-        assert_eq!(min_invalid(&[1, 2, 3, 100, 100, 4, 5]), 2);
-        assert_eq!(min_invalid(&[1, 2, 3, 4, 5, 100]), 1);
-        assert_eq!(min_invalid(&[100, 1, 2, 3, 4, 5, 100]), 2);
-        assert_eq!(min_invalid(&[100, 1, 2, 3, 100, 4, 5, 100]), 3);
-        assert_eq!(min_invalid(&[1, 3, 2, 3]), 1);
+        assert!(valid2(&[1, 2, 3, 4, 5], 1));
+        assert!(valid2(&[1, 2, 3, 100, 4, 5], 1));
+        assert!(!valid2(&[1, 2, 3, 100, 100, 4, 5], 1));
+        assert!(valid2(&[1, 2, 3, 4, 5, 100], 1));
+        assert!(!valid2(&[100, 1, 2, 3, 4, 5, 100], 1));
+        assert!(valid2(&[1, 3, 2, 3], 1));
+        assert!(!valid2(&[1, 3, 2, 0], 1));
+        assert!(!valid2(&[1, 3, 2, 100, 5], 1));
     }
 }
