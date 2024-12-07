0f66aa355f61fdf9fa82b3de1779c1eb3de256d7
Thomas Jollans <tjol@tjol.eu>
2024-12-07 09:37:09 +0100

    day 7 part 2

--- src/day07.rs
+++ src/day07.rs
@@ -1,19 +1,57 @@
 use std::str::FromStr;
 
-pub fn day07part1(input: &str) -> i64 {
-    let lines: Vec<&str> = input.lines().map(str::trim).filter(|l| !l.is_empty()).collect();
+pub fn day07part1(input: &str) -> u64 {
+    let lines: Vec<&str> = input
+        .lines()
+        .map(str::trim)
+        .filter(|l| !l.is_empty())
+        .collect();
     let eqs = parse_rules(&lines);
 
-    eqs.into_iter().filter_map(|eq| find_operators(eq.result, eq.operands[0], &eq.operands[1..]).is_some().then_some(eq.result)).sum()
+    eqs.into_iter()
+        .filter_map(|eq| {
+            find_operators(eq.result, eq.operands[0], &eq.operands[1..])
+                .is_some()
+                .then_some(eq.result)
+        })
+        .sum()
+}
+
+pub fn day07part2(input: &str) -> u64 {
+    let lines: Vec<&str> = input
+        .lines()
+        .map(str::trim)
+        .filter(|l| !l.is_empty())
+        .collect();
+    let eqs = parse_rules(&lines);
+
+    eqs.into_iter()
+        .filter_map(|eq| {
+            find_operators2(eq.result, eq.operands[0], &eq.operands[1..])
+                .is_some()
+                .then_some(eq.result)
+        })
+        .sum()
+}
+
+fn concat(a: u64, b: u64) -> u64 {
+    let mut factor = 10;
+    let mut n = b;
+    while n >= 10 {
+        n /= 10;
+        factor *= 10;
+    }
+
+    a * factor + b
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 struct BareEquation {
-    pub result: i64,
-    pub operands: Vec<i64>
+    pub result: u64,
+    pub operands: Vec<u64>,
 }
 
-fn find_operators(result: i64, first: i64, tail: &[i64]) -> Option<String> {
+fn find_operators(result: u64, first: u64, tail: &[u64]) -> Option<String> {
     if tail.is_empty() {
         if result == first {
             return Some("".to_string());
@@ -23,12 +61,38 @@ fn find_operators(result: i64, first: i64, tail: &[i64]) -> Option<String> {
     }
     let next = tail[0];
     if first * next <= result {
-        if let Some(ans) = find_operators(result, first*next, &tail[1..]) {
+        if let Some(ans) = find_operators(result, first * next, &tail[1..]) {
             return Some(ans);
         }
     }
     if first + next <= result {
-        return find_operators(result, first+next, &tail[1..]);
+        return find_operators(result, first + next, &tail[1..]);
+    }
+    None
+}
+
+fn find_operators2(result: u64, first: u64, tail: &[u64]) -> Option<String> {
+    if tail.is_empty() {
+        if result == first {
+            return Some("".to_string());
+        } else {
+            return None;
+        }
+    }
+    let next = tail[0];
+    if first * next <= result {
+        if let Some(ans) = find_operators2(result, first * next, &tail[1..]) {
+            return Some(ans);
+        }
+    }
+    if first + next <= result {
+        if let Some(ans) = find_operators2(result, first + next, &tail[1..]) {
+            return Some(ans);
+        }
+    }
+    let concatenated = concat(first, next);
+    if concatenated <= result {
+        return find_operators2(result, concatenated, &tail[1..]);
     }
     None
 }
@@ -39,9 +103,13 @@ impl FromStr for BareEquation {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let (left, right) = s.split_once(':').unwrap();
         let result = left.parse().unwrap();
-        let operands = right.trim().split_whitespace().map(|s| s.parse().unwrap()).collect();
-        
-        Ok(Self{result, operands})
+        let operands = right
+            .trim()
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect();
+
+        Ok(Self { result, operands })
     }
 }
 
@@ -51,7 +119,6 @@ fn parse_rules(lines: &[&str]) -> Vec<BareEquation> {
         .map(|line| line.parse().unwrap())
         .collect()
 }
-
 
 #[cfg(test)]
 mod test {
@@ -72,5 +139,10 @@ mod test {
     #[test]
     fn part1test() {
         assert_eq!(day07part1(TEST_INPUT), 3749);
+    }
+
+    #[test]
+    fn part2test() {
+        assert_eq!(day07part2(TEST_INPUT), 11387);
     }
 }
